84f91eab116c3a8816a6623217fa74660adc5c53
Ephraim Flores <ephraim.flores@ymail.com>
2025-03-24 14:45:25 -0700

    re-structure file

--- footer.js
+++
@@ -1,9 +0,0 @@
-// to be included in the head of all pages that need the navigation bar
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-container").innerHTML = data;
-        })
-        .catch(error => console.error("Error loading footer:", error));
-});
--- nav.js
+++
@@ -1,9 +0,0 @@
-// to be included in the head of all pages that need the navigation bar
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("nav.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-container").innerHTML = data;
-        })
-        .catch(error => console.error("Error loading navigation:", error));
-});
