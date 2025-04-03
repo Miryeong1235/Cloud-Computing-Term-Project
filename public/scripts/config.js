a121d60e2864e437116d4e4ac0ccfb3dd2a9f306
Darrel Tapilaha <tapsdarrel@gmail.com>
2025-04-02 23:00:25 -0700

    added elastic IP address to config.js

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,2 +1,2 @@
 const BASE_URL = "http://localhost:3000";
-// const BASE_URL = "http://35.95.158.95:3000"; // this is the EC2 instance
+// const BASE_URL = "http://35.95.158.95:3000"; // this is the EC2 elastic IP address
